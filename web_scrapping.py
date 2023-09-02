00595f6d6b931a500d6d88133b413eb324995842
Eduard-Valentin Cinca <eduard_cinca@yahoo.com>
2023-09-02 21:23:24 +0300

    saving a list only with track names and artists

--- web_scrapping.py
+++ web_scrapping.py
@@ -4,5 +4,11 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get('https://www.beatport.com/top-100')
 playlist = driver.find_elements(By.CLASS_NAME, 'sc-d187b421-4.bHvdSK')
+my_list = []
 for t in playlist:
-    print(t.text)
+    my_list.append(t.text)
+
+
+track_names = [(element.split('\n')[0] + ' ' + element.split('\n')[1]) for element in my_list]
+
+
